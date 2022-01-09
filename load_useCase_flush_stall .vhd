d932f0834223d270d6ce5c62eb95f9b04fdb3654
Esraa Gamal <69439108+esraagamal23@users.noreply.github.com>
2022-01-09 10:09:27 +0200

    Update load_useCase_flush_stall .vhd

--- load_useCase_flush_stall .vhd
+++ load_useCase_flush_stall .vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 Entity load_suse_case_flush_stalling is
 port(
 
-load_use_case,CLK,Pc_enable_in,D_E_buffer_flush_in,F_D_buffer_EN_in,D_E_buffer_EN_in:in std_logic;
+load_use_case,rst,CLK:in std_logic;
 Pc_enable_out,D_E_buffer_flush_out,F_D_buffer_EN_out,D_E_buffer_EN_out:out std_logic
 
 );
@@ -14,8 +14,10 @@ ARCHITECTURE load_suse_case_flush_stalling_structure of load_suse_case_flush_sta
 begin
 
 PROCESS (CLK)
-begin
-IF rising_edge(Clk)  THEN
+begin
+if rst='0' then
+
+IF rising_edge(Clk) THEN
 if(load_use_case='1') then
 D_E_buffer_flush_out<= '1';
 Pc_enable_out<='0';
@@ -26,7 +28,7 @@ D_E_buffer_flush_out<= '0';
 Pc_enable_out<='1';
 F_D_buffer_EN_out<='1';
 D_E_buffer_EN_out<='1';
-        
+End if;        
 End if;
 END IF;
 
@@ -39,4 +41,3 @@ END PROCESS;
 
 
 end load_suse_case_flush_stalling_structure;
-
